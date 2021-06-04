01408903c711c3fcc4c9c0f72fe280234a83c8ea
IreneTwo <83226683+irenetwoo@users.noreply.github.com>
2021-06-05 00:53:44 +0800

    Update file.py

--- file.py
+++ file.py
@@ -13,10 +13,7 @@ from detectron2.utils.logger import setup_logger
 setup_logger()
 from PIL import Image
 import torch, torchvision
-#assert torch.__version__.startswith("1.8") 
 
-st.write('hello')
-st.markdown('Guess the object Names')
 st.markdown("""<style> .font {font-size:50px ; font-family: 'Cooper Black'; color: #FF9633;}</style>""", unsafe_allow_html=True)
 st.markdown('<p class="font">Guess the object Names</p>', unsafe_allow_html=True)
 st.markdown('** Can you name the items in the picture correctly? **')
@@ -67,11 +64,14 @@ def main():
           st.markdown('<p class="font"> TADAA ! </p>', unsafe_allow_html=True)
           st.image(out_image, caption=' After ', use_column_width=True)   
           st.markdown(' **Can you find any item(s) labelled wrongly in the picture ?** ')
+          st.markdown('')
+          st.markdown('Made by: [Irene Too](https://github.com/irenetwoo/Streamlit-Object-Detector)')
+          st.write('This app uses a part of detection code from  Javier Esteve repo https://github.com/xavialex/Detectron2-Instance-Segmentation. Many thanks to this project.')
           
 if __name__ == '__main__':
     main()
 
-st.write('Image taken from Unsplashed https://unsplash.com/photos/UUcnOlfMePU')
-st.markdown('Made by: [Irene Too](https://github.com/irenetwoo/Streamlit-Object-Detector)')
-st.write('This app uses a part of detection code from  Javier Esteve repo https://github.com/xavialex/Detectron2-Instance-Segmentation. Many thanks to this project.')
+    
+
+
 
