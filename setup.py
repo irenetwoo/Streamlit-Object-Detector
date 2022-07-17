bd51594a44739bcc90f98f15bc802c782c1dc509
IreneToo <83226683+irenetwoo@users.noreply.github.com>
2022-07-17 11:54:35 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,5 @@
+setup(name="git+https://github.com/facebookresearch/detectron2.git",
+    install_require={
+        ["gcc7==0.0.7" , "opencv-python-headless==4.5.2.52", "pycocotools==2.0.2", "torch==1.8.1" , "torchvision==0.9.1", "torchaudio===0.8.1"]
+    }
+)
