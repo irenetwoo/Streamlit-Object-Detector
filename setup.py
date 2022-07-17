30a7f679dae7c00cae5c12f5c4572904fe6c26fd
IreneToo <83226683+irenetwoo@users.noreply.github.com>
2022-07-17 15:10:04 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 setup(name="git+https://github.com/facebookresearch/detectron2.git",
     install_require={
-        ["gcc7==0.0.7" , "opencv-python-headless==4.5.2.52", "pycocotools==2.0.2", "torch==1.12.0" , "torchvision==0.13.0", "torchaudio==0.16.0"]
+        ["gcc7==0.0.7" , "opencv-python-headless==4.5.2.52", "pycocotools==2.0.2", "torchvision==0.13.0", "torchaudio==0.16.0"]
     }
 )
